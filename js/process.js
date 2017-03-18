ea324d597713f64340eb7d49d3999eec30a6d6d3
Nishisonic <graphic.extra005@gmail.com>
2017-03-18 09:31:10 +0900

    スロット機数はリセットかからないようにした

--- js/process.js
+++ js/process.js
@@ -235,7 +235,7 @@ function initialize(){
     $('.parseEnemy').show();
   }
   $('#formationBox').children().prop('selectedIndex', 0);
-  $('#slotNumSpinner').val(0);
+  // $('#slotNumSpinner').val(0);
   $('#tyku_cutinBox').children().prop('selectedIndex', 0);
   for(let i = 1;i <= 2;i++){
     for(let j = 1;j <= 6;j++){
